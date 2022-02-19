a9fafc7806fb4e8bfa307d465963e088f53b3a5f
tejal-1303 <79006362+tejal-1303@users.noreply.github.com>
2022-02-19 23:47:44 +0530

    Day 15 solutions added

---
+++ day-15/index.js
@@ -0,0 +1,11 @@
+const isPangram = (input) => {
+  input = input.toUpperCase().split("");
+  for (let i = 65; i < 91; i++) {
+    let char = String.fromCharCode(i);
+    if (input.includes(char)) continue;
+    else return false;
+  }
+  return true;
+};
+
+console.log(isPangram("The quick brown fox jumps over the lazy dog."));
